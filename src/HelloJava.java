65974fd14633b62d55a853df13e7eed2c9a06db3
fortune-man <wnguddl96@knou.ac.kr>
2025-02-23 22:47:10 +0900

    Initial commit

---
+++ src/HelloJava.java
@@ -0,0 +1,11 @@
+
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello world java1");
+        System.out.println("hello world java2");
+        System.out.println("hello world java3");
+
+
+    }
+}
---
+++ src/scanner/Scanner1.java
@@ -0,0 +1,11 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("start");
+    }
+}
