7dd1f2573c1c55c8a9d4ed1b24daadb998bddb8c
fortune-man <wnguddl96@knou.ac.kr>
2025-02-23 22:49:58 +0900

    refactor : init HelloJava

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -2,9 +2,7 @@
 
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("hello world java1");
-        System.out.println("hello world java2");
-        System.out.println("hello world java3");
+        System.out.println("hello world java");
 
 
     }
