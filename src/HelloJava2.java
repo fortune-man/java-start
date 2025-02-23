523cd08ebc3c3fa73be372adc7c7d02a279526c0
fortune-man <wnguddl96@knou.ac.kr>
2025-02-23 22:54:41 +0900

    refactor : add magic number for develop

--- src/HelloJava2.java
+++ src/HelloJava2.java
@@ -1,9 +1,11 @@
 public class HelloJava2 {
 
+    private static int ITERATE = 10;
+
     public static void main(String[] args) {
         String helloword = "\nhello java";
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 1; i < ITERATE+1; i++) {
             System.out.print(helloword + i) ;
         }
     }
