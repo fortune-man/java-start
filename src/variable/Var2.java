6f746b3f1206c3465d11f3e0bf34e474d47bf9a9
fortune-man <wnguddl96@knou.ac.kr>
2025-02-25 00:14:07 +0900

    refactor : a 값 변경

--- src/variable/Var2.java
+++ src/variable/Var2.java
@@ -4,7 +4,7 @@ public class Var2 {
 
   public static void main(String[] args) {
     int a;
-    a = 10;
+    a = 20;
     System.out.println(a);
     System.out.println(a);
     System.out.println(a);
