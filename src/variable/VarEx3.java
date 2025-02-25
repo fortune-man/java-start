5910492704a0179f71c17f9ac663333553ada6b5
fortune-man <wnguddl96@knou.ac.kr>
2025-02-25 21:34:42 +0900

    feat(VarEx3) : long, boolean 데이터 타입 연습

---
+++ src/variable/VarEx3.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class VarEx3 {
+
+  public static void main(String[] args) {
+    long mil = 10000000000L;
+    boolean flag = true;
+
+    System.out.println(mil);
+    System.out.println(flag);
+  }
+
+}
