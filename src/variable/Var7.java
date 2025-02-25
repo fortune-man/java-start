ecb35cd604aa50903d2265a3bd7e8d71e4fd95e4
fortune-man <wnguddl96@knou.ac.kr>
2025-02-25 21:12:56 +0900

    refactor : 리터럴 설명 주석 추가

--- src/variable/Var7.java
+++ src/variable/Var7.java
@@ -3,12 +3,18 @@ package variable;
 public class Var7 {
 
   public static void main(String[] args) {
-    int a = 100;
-    double b = 10.5;
-    boolean c = true;
-    char d = 'A';
-    String e = "Hello world";
+    int a = 100; // 정수
+    double b = 10.5; // 실수
+    boolean c = true; // 불리언 boolean
+    char d = 'A'; // 문자 하나
+    String e = "Hello world"; // 문자열을 다루기 위한 데이터 타입
+    // 코드에서 개발자가 직접 적은 100, 10.5, true, 'A', "Hello Java"와 같은 값을 리터럴(literal)이라고 부름
+    // 개발자가 직접 입력한 고정된 값. 리터럴
     System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
   }
 
 
