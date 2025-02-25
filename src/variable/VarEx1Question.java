caefccf0da94ef7de3778730c635f56efb21cff3
fortune-man <wnguddl96@knou.ac.kr>
2025-02-25 21:31:52 +0900

    feat : VarEx1 덧셈 뺄셈 곱셈

---
+++ src/variable/VarEx1Question.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx1Question {
+
+  public static void main(String[] args) {
+    System.out.println(4+3);
+    System.out.println(4-3);
+    System.out.println(4*3);
+  }
+
+}
