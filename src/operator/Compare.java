8d306f017dbe58c81a25cd660b41cb2dad440898
fortune-man <wnguddl96@knou.ac.kr>
2025-02-26 22:38:22 +0900

    feat : Compare

---
+++ src/operator/Compare.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class Compare {
+
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 3;
+
+    System.out.println(a == b);
+    System.out.println(a != b);
+    System.out.println(a>b);
+    System.out.println(a<b);
+    System.out.println(a >= b);
+    System.out.println(a <= b);
+
+    boolean result = a == b;
+    System.out.println(result);
+  }
+
+}
