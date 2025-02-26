fc8360d39ef4562f28daa68e1ba389d0de4303cd
fortune-man <wnguddl96@knou.ac.kr>
2025-02-26 22:30:40 +0900

    refactor : Operator2

--- src/operator/Operator2.java
+++ src/operator/Operator2.java
@@ -5,6 +5,19 @@ public class Operator2 {
   public static void main(String[] args) {
     String result = "hello " + "world!";
     System.out.println(result);
-  }
 
+    String string1 = "string1";
+    String string2 = "string2";
+
+    String result2 = string1 + string2;
+    System.out.println(result2);
+
+    String result3 = "a + b = " + 10;
+    System.out.println(result3);
+
+    int num = 20;
+    String str = "a+b=";
+    String result4 = str + num;
+    System.out.println(result4);
+  }
 }
