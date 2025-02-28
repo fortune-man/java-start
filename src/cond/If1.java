75b13f791e859bee0749086c976d794727370bf9
fortune-man <wnguddl96@knou.ac.kr>
2025-02-28 21:31:36 +0900

    feat : If1

---
+++ src/cond/If1.java
@@ -0,0 +1,17 @@
+package cond;
+
+public class If1 {
+
+  public static void main(String[] args) {
+    int age = 20;
+
+    if (age >= 18) {
+      System.out.println("adult 성인용😏");
+    }
+
+    if (age < 18) {
+      System.out.println("kids 청소년");
+    }
+  }
+
+}
