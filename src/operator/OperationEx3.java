968f9f30218fa437da6bb01fc98759e0b105b68c
fortune-man <wnguddl96@knou.ac.kr>
2025-02-28 21:28:52 +0900

    refactor : 자주 사용하는 연산자 주석 추가

--- src/operator/OperationEx3.java
+++ src/operator/OperationEx3.java
@@ -8,4 +8,13 @@ public class OperationEx3 {
     System.out.println(result);
   }
 
+  /**
+   * 자주 사용하는 연산자
+   * 산술 연산 + - * / ^
+   * 증가 및 감소 ++ --
+   * 비교 == != > < >= <=
+   * 논리 && || !
+   * 대입 = += -= *= /= %=
+   */
+
 }
