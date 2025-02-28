eaaab1fdf15b6011eda9d99e5a95fb017762c2e3
fortune-man <wnguddl96@knou.ac.kr>
2025-02-28 21:48:31 +0900

    refactor : 논리 오류, 초등학생 조건문 age <= 10 -> 13

--- src/cond/If3.java
+++ src/cond/If3.java
@@ -14,7 +14,7 @@ public class If3 {
 
   private static void printStatusUser(int age) {
     boolean kids = 7 >= age;
-    boolean elementary = age >= 8 && age <= 10;
+    boolean elementary = age >= 8 && age <= 13;
     boolean middle = 14 <= age && age <= 16;
     boolean high = 17 <= age && age <= 19;
     boolean adult = age >= 20;
@@ -22,16 +22,16 @@ public class If3 {
     if (kids) {
       System.out.println("미취학");
     }
-    if (elementary) {
+    else if (elementary) {
       System.out.println("초등학생");
     }
-    if (middle) {
+    else if (middle) {
       System.out.println("중학생");
     }
-    if (high) {
+    else if (high) {
       System.out.println("고등학생");
     }
-    if (adult) {
+    else if (adult) {
       System.out.println("성인");
     }
   }
