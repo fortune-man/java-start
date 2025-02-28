24d054feb0dafe0244fdc67b8aa1ccb480f8ed30
fortune-man <wnguddl96@knou.ac.kr>
2025-02-28 21:32:48 +0900

    feat : If2

---
+++ src/cond/If2.java
@@ -0,0 +1,13 @@
+package cond;
+
+public class If2 {
+  public static void main(String[] args) {
+    int age = 20;
+    if (age >= 18) {
+      System.out.println("adult 성인용😏");
+    } else {
+      System.out.println("kids 청소년");
+    }
+  }
+
+}
