8c9566674a51e04759bf4a1c6490dad2930754d6
fortune-man <wnguddl96@knou.ac.kr>
2025-02-28 21:44:45 +0900

    feat(If3) : If3 연령에 따라 다른 메시지를 출력하는 프로그램

---
+++ src/cond/If3.java
@@ -0,0 +1,39 @@
+package cond;
+
+import java.util.Scanner;
+
+public class If3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int age = scanner.nextInt();
+    printStatusUser(age);
+
+
+
+  }
+
+  private static void printStatusUser(int age) {
+    boolean kids = 7 >= age;
+    boolean elementary = age >= 8 && age <= 10;
+    boolean middle = 14 <= age && age <= 16;
+    boolean high = 17 <= age && age <= 19;
+    boolean adult = age >= 20;
+
+    if (kids) {
+      System.out.println("미취학");
+    }
+    if (elementary) {
+      System.out.println("초등학생");
+    }
+    if (middle) {
+      System.out.println("중학생");
+    }
+    if (high) {
+      System.out.println("고등학생");
+    }
+    if (adult) {
+      System.out.println("성인");
+    }
+  }
+
+}
