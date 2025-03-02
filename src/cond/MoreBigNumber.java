70d29ba04a6690a37f418fe4a446a9e4b013f972
fortune-man <wnguddl96@knou.ac.kr>
2025-03-03 00:31:08 +0900

    feat(MoreBigNumber) : 더 큰 숫자 찾기 틀린 코드 추가

---
+++ src/cond/MoreBigNumber.java
@@ -0,0 +1,18 @@
+package cond;
+
+/**
+ *문제: 더 큰 숫자 찾기
+ * 여러분은두개의정수변수 `a` 와 `b` 를가지고있다. `a` 의값은 `10` 이고, `b` 의값은 `20` 이다.삼항연산자를사용하여
+ * 두 숫자 중 더 큰 숫자를 출력하는 코드를 작성하자.
+ */
+public class MoreBigNumber {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+
+    if (a > b ? 10 : 20) {
+      System.out.println(b);
+    }
+  }
+
+}
