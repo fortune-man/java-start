0380dc8b45cd5419097a673998b7586aa4fbb3d2
fortune-man <wnguddl96@knou.ac.kr>
2025-03-03 00:15:42 +0900

    refactor : 평점 요

--- src/cond/Recommend.java
+++ src/cond/Recommend.java
@@ -9,21 +9,21 @@ import java.util.Scanner;
  * 고질라 - 평점7
  *  평점 변수는 `double rating` 을 사용하세요. `if` 문을 활용해서 문제를 풀자.
  */
+
+
 public class Recommend {
- //
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    double rating = scanner.nextInt();
+    double rating = 8.5;
 
     if (rating <= 7) {
-      System.out.println("고질라 - 평점7");
+      System.out.println("고질라");
     }
     if (rating <= 8) {
-      System.out.println("토이 스토리 - 평점8");
+      System.out.println("토이 스토리");
     }
     if (rating <= 9) {
-      System.out.println("어바웃타임 - 평점9");
+      System.out.println("어바웃타임");
     }
-    System.out.println("요청하신 평점이 등록된 영화가 없습니다.");
+
   }
 }
