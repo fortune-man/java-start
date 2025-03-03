3343093282fa738fdd880617d4d1291fbfbaf0db
fortune-man <wnguddl96@knou.ac.kr>
2025-03-03 21:34:42 +0900

    feat(ODDEVEN) : 짝수 홀수 계산기

---
+++ src/cond/ODDEVEN.java
@@ -0,0 +1,43 @@
+package cond;
+
+import java.util.Scanner;
+
+/**
+ * 문제: 홀수 짝수 찾기
+ * 정수 `x` 가 주어지면 `x` 가 짝수이면 "짝수"를, `x` 가 홀수이면 "홀수"를 출력하는 프로그램을 작성하자
+ * 삼항 연산자를 사용해야 한다.
+ * 참고로 `x % 2` 를사용하면홀수,짝수를쉽게계산할수있다.
+ */
+public class ODDEVEN {
+  private static String even = "짝수";
+  private static String odd = "홀수";
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+
+    boolean printEven = true;
+    boolean printOdd = true;
+
+    boolean isEven = x % 2 == 0;
+
+    if ( isEven == true ? printEven : printOdd) {
+      if (printEven) {
+        isPrintEven(printEven);
+      }
+      if (printOdd) {
+        isPrintOdd(printOdd);
+      }
+    }
+    System.out.println(odd);
+    scanner.close();
+  }
+
+  private static void isPrintOdd(boolean printOdd) {
+    System.out.println(odd);
+  }
+
+  private static void isPrintEven (boolean printEven) {
+    System.out.println(even);
+  }
+}
