abe2b8f0ca81c78026947d0e07c6258875784425
fortune-man <wnguddl96@knou.ac.kr>
2025-03-07 23:32:14 +0900

    feat : DoWhile2

--- src/loop/While2_3.java
+++ src/loop/While2_3.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class While2_3 {
 // 충분한 분석 필요
   public static void main(String[] args) {
-  // Java로 1~10 더하는 계산기 완성
-    int n = 1;
-    int sum = 0;
-    while (n<=10) {
-      System.out.println("덧셈 수=" + n);
-      sum+=n;
-      n++;
-    }
-    System.out.println("계산 결과=" + sum);
-  }
+  // 입력 받아서 더하기 빼기 구현
+    Scanner scanner = new Scanner(System.in);
+    int first = scanner.nextInt();
+    String operate = scanner.next();
+    int next = scanner.nextInt();
+    int result = 0;
 
+    }
 }
