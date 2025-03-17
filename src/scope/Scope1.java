32800766d18329550ab48078470145e822d3fde0
fortune-man <wnguddl96@knou.ac.kr>
2025-03-17 23:11:45 +0900

    feat Scope2

--- src/scope/Scope1.java
+++ src/scope/Scope1.java
@@ -3,7 +3,7 @@ package scope;
 public class Scope1 {
 
   public static void main(String[] args) {
-    int m = 10; // m 생존 시작
+    int m = 10; // m 생존 시작 : 메인 블록 안에서 선언 ->
     if (true) {
       int x = 20; // X 생존 시작
       System.out.println("if m = " + m);
