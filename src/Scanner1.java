ad7094e93d98d4685f2e4ae4891ebf7bb8a0a1ef
fortune-man <wnguddl96@knou.ac.kr>
2025-03-22 23:47:03 +0900

    feat(Scanner1) : 조금 더 흥미를 느끼고자 scanner 놀이 코드 추가. scanner 입력 과정. 문자열. 정수. 실수 입출력

---
+++ src/Scanner1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Scanner1 {
+
+  public static void main(String[] args) {
+
+    // 사용자 입력을 받기 위한 System.in 주입한 Scanner 객체 사용?
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("문자열 입력: ");
+
+    // 스캐너를 통해 문자열 입력
+    String next = scanner.next();
+    String nextLine = scanner.nextLine();
+
+    // 차이가 뭘까?
+    System.out.println("첫번째 입력: " + next);
+    System.out.println("두번째 입력: " + nextLine);
+
+    // 정수 입력. 출력
+    System.out.println("정수를 입력하세요");
+    int intValue = scanner.nextInt();
+    System.out.println("입력한 정수 " + intValue);
+
+    // 실수 입력. 출력
+    System.out.println("실수를 입력하세요");
+    double doubleValue = scanner.nextDouble();
+    System.out.println("입력한 실수 " + doubleValue);
+
+
+    // 정수 입력 . 출력
+
+    // 실수 입력 . 출력
+  }
+
+}
--- src/casting/Castring3.java
+++ src/casting/Castring3.java
@@ -1,2 +1,17 @@
-package casting;public class Castring3 {
+package casting;
+
+public class Castring3 {
+
+  public static void main(String[] args) {
+    long maxIntValue = 2147483647;
+    long maxIntOver = 2147483648L;
+    int intValue = 0;
+
+    intValue = (int) maxIntValue;
+    System.out.println("maxIntValue casting= " + maxIntValue);
+
+    intValue = (int) maxIntOver;
+    System.out.println("maxIntOver casting= " + maxIntOver);
+  }
+
 }
