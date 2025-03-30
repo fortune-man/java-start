8532ae375c5a7bd1fec797ff5f7ee59875b2054d
fortune-man <wnguddl96@knou.ac.kr>
2025-03-30 13:06:39 +0900

    feat(ScannerWhileEx2) : 상품 가격 계산 메모리 저장 문제 scanner.close()로 해결 시도 1번한 결과 IllegalStateException 1번

--- src/scanner/ScannerWhileEx2.java
+++ src/scanner/ScannerWhileEx2.java
@@ -6,22 +6,29 @@ public class ScannerWhileEx2 {
   static int price;
   static int quantity;
   static int amount;
+  static final int EXIT = -1;
 
-  public ScannerWhileEx2(int amount, int price, int quantity) {
-    this.amount = amount;
-    this.price = price;
-    this.quantity = quantity;
-  }
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    price = scanner.nextInt();
-    quantity = scanner.nextInt();
+    boolean exit = false;
 
-    amount = price * quantity;
+    // 메모리 저장 문제
+    while(!exit) {
+      System.out.println(String.format("상품의 가격을 입력하세요: (-1을 입력하면 종료): %d", price));
+      price = scanner.nextInt();
+      System.out.println(String.format("구매하려는 수량을 입력하세요: %d", quantity));
+      quantity = scanner.nextInt();
 
-    System.out.println(String.format("상품의 가격을 입력하세요: (-1을 입력하면 종료): %d", price));
-    System.out.println(String.format("구매하려는 수량을 입력하세요: %d", quantity));
-    System.out.println(String.format("총 비용: %d달러", amount));
+      if(price == -1 || quantity == -1) {
+        System.out.println("프로그램을 종료합니다.");
+        exit = true;
+      }
+      amount = price * quantity;
+      System.out.println(String.format("총 비용: %d원", amount));
+      scanner.close(); // IllegalStateException
+
+    }
+
   }
 }
