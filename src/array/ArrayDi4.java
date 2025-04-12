4a90e3e4ce15984d108bf49d2f05ce2781486fd1
fortune-man <wnguddl96@knou.ac.kr>
2025-04-12 23:33:15 +0900

    feat(ArrayEx2) : 정수 입력 출력

--- src/array/ArrayDi4.java
+++ src/array/ArrayDi4.java
@@ -1,2 +1,17 @@
-package array;public class ArrayDi4 {
+package array;
+
+public class ArrayDi4 {
+
+  public static void main(String[] args) {
+    int[][] arr = new int[2][3];
+
+    int i = 1;
+
+    for (int row = 0; row < arr.length; row++) {
+      for (int col = 0; col < arr[row].length; col++) {
+        arr[row][col] = i++;
+      }
+    }
+  }
+
 }
--- src/array/EnhancedFor1.java
+++ src/array/EnhancedFor1.java
@@ -1,2 +1,24 @@
-package array;public class EnhancedFor1 {
+package array;
+
+public class EnhancedFor1 {
+
+  public static void main(String[] args) {
+    int[] numbers = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < numbers.length; i++) {
+      int number = numbers[i];
+      System.out.println(number);
+    }
+
+    for(int i = 1; i < numbers.length; i++){
+      System.out.println("number" + i + "번의 결과는" + numbers[i]);
+    }
+
+    System.out.println("===================== \n");
+    for (int number : numbers) {
+      System.out.println("number" + number + "번의 결과는" + number);
+
+    }
+  }
+
 }
