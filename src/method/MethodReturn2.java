a079aca96a8f8d8c7ed21c5b7f6b003f729a8a41
fortune-man <wnguddl96@knou.ac.kr>
2025-04-15 14:13:11 +0900

    feat(MethodReturn2) : 반환 타입 조건문 탈출 케이스 학습을 위한 checkAge() 메서드 학습

---
+++ src/method/MethodReturn2.java
@@ -0,0 +1,24 @@
+package method;
+
+public class MethodReturn2 {
+
+  public static void main(String[] args) {
+    checkAge(6);
+    checkAge(30);
+    checkAge(38);
+    checkAge(41);
+    checkAge(41);
+    checkAge(61);
+
+  }
+
+  public static void checkAge(int age) {
+    if (age < 18) {
+      System.out.println(age + "살, 미성년자는 출입이 불가능합니다.");
+      return;
+    }
+
+    System.out.println(age + "살, 입장하세요");
+  }
+
+}
