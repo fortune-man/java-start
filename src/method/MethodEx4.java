fa7f2d6ca14a5739c76caa4a277181b749dc19e1
fortune-man <wnguddl96@knou.ac.kr>
2025-04-15 15:44:35 +0900

    refactor(MethodEx4) : 잔액 변경 갱신 안되는 문제가 있어 배열로 상태 변경 적용 가능하게 만들어 적용 후 확인까지 개선

--- src/method/MethodEx4.java
+++ src/method/MethodEx4.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class MethodEx4 {
 
   public static void main(String[] args) {
-    int balance = 0;
+
     Scanner scanner = new Scanner(System.in);
+    int[] database = new int[1];
+    int balance = database[0];
 
     while (true) {
       String boarderline = "--------------------------------- \n";
@@ -14,30 +16,36 @@ public class MethodEx4 {
       System.out.println(boarderline + ui + boarderline);
       System.out.println("선택: ");
       int choose = scanner.nextInt();
+
       if (choose == 1) {
         System.out.println("입금액을 입력하세요:");
-        balance = scanner.nextInt();
+        balance += scanner.nextInt();
         String message = String.format("%d원을 입금하였습니다. 현재 잔액: %d원", balance, balance);
         System.out.println(message);
         continue;
       }
+
       if (choose == 2) {
         System.out.println("출금액을 입력하세요:");
         int withDrawl = scanner.nextInt();
         int state = balance - withDrawl;
+        balance -= withDrawl;
         String message = String.format("%d원을 출금하였습니다. 현재 잔액: %d원", withDrawl, state);
         System.out.println(message);
         continue;
       }
+
+      // 잔액 변경 갱신 안되는 문제
+
       if (choose == 3) {
         System.out.println(String.format("현재 잔액: %d원", balance));
         continue;
       }
+
       if (choose == 4) {
         System.out.println("시스템을 종료합니다.");
         break;
       }
-
     }
   }
 
