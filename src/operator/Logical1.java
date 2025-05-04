bbdc54d981d2288fd9ff761e9e873851510d727a
fortune-man <wnguddl96@knou.ac.kr>
2025-05-04 22:34:05 +0900

    refactor : 메서드 분리

--- src/operator/Logical1.java
+++ src/operator/Logical1.java
@@ -4,9 +4,9 @@ public class Logical1 {
 
   public static void main(String[] args) {
     System.out.println("&&: AND 연산");
-    System.out.println(true && true);
-    System.out.println(true && false);
-    System.out.println(false && false);
+    System.out.println(isTrue());
+    System.out.println(trueOrFalse());
+    System.out.println(isFalse());
 
     System.out.println("|| : OR 연산");
     System.out.println(true || true);
@@ -24,6 +24,18 @@ public class Logical1 {
     System.out.println(a || b); // true, 둘 중 하나가 true
     System.out.println(!a);
     System.out.println(!b);
+  }
+
+  private static boolean isFalse() {
+    return false && false;
+  }
+
+  private static boolean trueOrFalse() {
+    return true && false;
+  }
+
+  private static boolean isTrue() {
+    return true && true;
   }
 
 }
