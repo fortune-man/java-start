7e2c916040a4021ed143903b048f9dd324c96a57
fortune-man <wnguddl96@knou.ac.kr>
2025-05-09 00:18:49 +0900

    refactor : 채무 이행 필요

--- src/operator/Assign.java
+++ src/operator/Assign.java
@@ -5,6 +5,8 @@ public class Assign {
   public static void main(String[] args) {
     int a = 5;
     System.out.println(a);
+    int goal = 3000000;
+    System.out.println(String.format("solve dept = %d", goal));
   }
 
 }
